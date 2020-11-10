92a3929ee9fd3fe9b375c9b49d29cde91c8c2041
Jihyoung <emili1001@naver.com>
2020-11-10 12:40:58 +0900

    route

--- conference/src/Root.js
+++ conference/src/Root.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import App from '/Users/parkjiho/Desktop/2020-2-OSSP1-WebRTC-2/conference/src/App';
+import App from './App';
 
 const Root = () => (
     <BrowserRouter>
