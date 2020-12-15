0d54a583ebf503f21a2a3a79b58c744f65cad15d
zio <jiho8345@naver.com>
2020-12-15 13:36:36 +0900

    임시

--- conference/src/Components/Blackboard/index.js
+++
@@ -1,1 +0,0 @@
-export { default as Blackboard } from './Blackboard';
--- conference/src/Root.js
+++ conference/src/Root.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { Blackboard } from './Components/Blackboard';
+//import { Blackboard } from './Components/Blackboard';
 
 const Root = () => (
     <BrowserRouter>
