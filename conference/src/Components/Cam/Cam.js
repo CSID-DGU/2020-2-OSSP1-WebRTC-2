433a944b09fa120496d26a645c195bf63c0fb716
zio <jiho8345@naver.com>
2020-12-15 23:06:14 +0900

    turn server

--- conference/src/Components/Cam/Cam.js
+++ conference/src/Components/Cam/Cam.js
@@ -32,6 +32,11 @@ class Cam extends Component {
         "iceServers": [
           {
             urls : 'stun:stun.l.google.com:19302'
+          },
+          {
+            urls:"turn:numb.viagenie.ca",
+            credential:"muazkh",
+            username:"webrtc@live.com"
           }
         ]
       },
